897fcd9158be075b22ca30c3dc224edd8e1ae17f
SukhdeepSingh1234 <ss6540029@gmail.com>
2022-01-17 23:05:31 +0530

    node js first project commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path=require("path");
+const app=express();
+const port =8000;
+
+//Express specific stuff
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+//Pug specific stuff
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/', (req,res)=>{
+    res.status(200).render('index');
+
+});
+
+app.listen(port , ()=>{
+    console.log(`The Port started at ${port}`);
+
+})

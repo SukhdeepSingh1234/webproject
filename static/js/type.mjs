6145cce48796e53c94cb20f92c760aeb0b2ffda9
SukhdeepSingh1234 <ss6540029@gmail.com>
2022-08-26 21:45:04 +0530

    hello

---
+++ static/js/type.mjs
@@ -0,0 +1,8 @@
+// import Typed  from 'https://cdn.jsdelivr.net/npm/typed.js@2.0.12';
+// var typed = new Typed(".auto",options);
+// var options = {
+//     strings:["Bhangra", "Hip-Hop","Salsa","Classical Dance", "Folk Dance"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     loop:true
+//   };
